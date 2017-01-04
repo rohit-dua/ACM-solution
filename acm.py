9cb92314cea40850ab68e7ae9e66b7a881df0507
Rohit Dua <8ohit.dua@gmail.com>
2017-01-05 00:35:21 +0530

    New Changes

--- acm.py
+++ acm.py
@@ -35,11 +35,6 @@ class ACM(object):
 		self.number_of_days = number_of_days
 		self.machines_per_day= dict()
 		self.sale_days = list()
-		# Memoization Save for Dynamic Programming
-		# Top Down approach
-		self.DP_save = dict()
-		for i in range(1,self.number_of_days+1):
-			self.DP_save[i] = dict()
 
 	def add_machine_sale(self, sale_day, cost_price, selling_price, profit_per_day):
 		"""Add machine to sale"""
@@ -58,55 +53,47 @@ class ACM(object):
 	def calculate_max_profit(self):
 		"""Function to call to calculate max profit"""
 		self.sale_days = self.get_sale_days()
-		max_profit = self.execute_calculate_max_profit(day=1, cash=self.balance, current_machine=None)
+		max_profit = self.execute_calculate_max_profit(day_index=0, cash=self.balance, current_machine=None)
 		return max_profit
 
-	def execute_calculate_max_profit(self, day, cash, current_machine):
+	def execute_calculate_max_profit(self, day_index, cash, current_machine):
 		# Each day 2 options: Sell/Buy or Stay
 		# Can skip days of non sale
-		if day>self.number_of_days:
+		if day_index >= len(self.sale_days):
+			days_left = self.number_of_days - self.sale_days[-1]
 			if current_machine is not None:
-				cash += current_machine.selling_price
-				current_machine = None
+				cash = cash + (days_left * current_machine.profit_per_day)
+				cash = cash + current_machine.selling_price
 			return cash
-		if current_machine is not None and current_machine in self.DP_save[day].keys():
-			return self.DP_save[day][current_machine]
-		if day not in self.sale_days:
-			#stay and increase profit
-			if current_machine is None:
-				value = self.execute_calculate_max_profit(day+1, cash, current_machine)
-			else:
-				value = self.execute_calculate_max_profit(day+1, cash + current_machine.profit_per_day, current_machine)
-			self.DP_save[day][current_machine] = value
-			return value
+		day = self.sale_days[day_index]
+		if day_index != 0 and current_machine is not None:
+			previous_day = self.sale_days[day_index - 1]
+			cash = cash + (day - previous_day - 1)*current_machine.profit_per_day
+		# Each selling day 2 options: Sell/Buy(for each item) or Stay
+		#stay
+		if current_machine is None:
+			stay_result = self.execute_calculate_max_profit(day_index+1, cash, current_machine)
 		else:
-			# Each selling day 2 options: Sell/Buy(for each item) or Stay
-			#stay
-			if current_machine is None:
-				stay_result = self.execute_calculate_max_profit(day+1, cash, current_machine)
-			else:
-				stay_result = self.execute_calculate_max_profit(day+1, cash + current_machine.profit_per_day, current_machine)
-			self.DP_save[day][current_machine] = stay_result
-			#Sell and buy
-			#sell
-			if current_machine is not None:
-				cash += current_machine.selling_price
-				current_machine = None
-			#For each buy, we have multiple options of machines.
-			results = list()
-			for machine in self.machines_per_day[day]:
-				if machine.cost_price <= cash:
-					value = self.execute_calculate_max_profit(day+1, cash-machine.cost_price, machine)
-					self.DP_save[day][machine] = value
-					results.append(value)
-			#check if no machine bought, then no need to sell
-			if results == []:
-				return stay_result
-			else:
-				#Get max of selling or staying
-				selling_result = max(results)
-				max_result = max(stay_result, selling_result)
-				return max_result
+			stay_result = self.execute_calculate_max_profit(day_index+1, cash + current_machine.profit_per_day, current_machine)
+		#Sell and buy
+		#sell
+		if current_machine is not None:
+			cash += current_machine.selling_price
+			current_machine = None
+		#For each buy, we have multiple options of machines.
+		results = list()
+		for machine in self.machines_per_day[day]:
+			if machine.cost_price <= cash:
+				value = self.execute_calculate_max_profit(day_index+1, cash-machine.cost_price, machine)
+				results.append(value)
+		#check if no machine bought, then no need to sell
+		if results == []:
+			return stay_result
+		else:
+			#Get max of selling or staying
+			selling_result = max(results)
+			max_result = max(stay_result, selling_result)
+			return max_result
 
 
 def main():
